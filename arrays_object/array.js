0edfefd753b598dc12ec2965c86786aba78c96ef
sdhxm <syakiladelinahadisty@gmail.com>
2025-03-03 23:01:01 +0700

    Add files via upload

---
+++ arrays_object/array.js
@@ -0,0 +1,2 @@
+let mobil = ["Bugatti", "Porsche", "Pagani"];
+console.log(mobil[0]);
---
+++ arrays_object/object.js
@@ -0,0 +1,4 @@
+const orang = {nama:"syakila", umur:"18", tanggal_lahir:"18 agustus 2006"};
+
+console.log(orang.nama);
+
