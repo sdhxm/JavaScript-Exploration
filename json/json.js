f9d9e9d0c03f9b38d68ef88a5e16729d7c532f01
sdhxm <syakiladelinahadisty@gmail.com>
2025-03-28 17:00:54 +0700

    Add files via upload

--- json/json.js
+++ json/json.js
@@ -1,1 +1,4 @@
-"json" 
+let jsonData = '[{"name": "syakila", "age": 19}, {"name": "delina", "age": 18}]';
+let parsedData = JSON.parse(jsonData); // Mengubah JSON string menjadi array of objects
+
+console.log(parsedData[0].name); 
